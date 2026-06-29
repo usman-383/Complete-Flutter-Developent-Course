92a1e6116af0f804cbc58fd5ed3d856ceb2a3572
usman-383 <usmankhan.x383@gmail.com>
2026-06-29 09:15:45 +0500

    Create 10-Keyword.dart

---
+++ Section-3 Dart Programming Fundamental/10-Keywords.dart
@@ -0,0 +1,22 @@
+void main() {
+  /*
+  Keywords:
+          Pre-define and reserve words that conway a meaningful message to compiler
+          In dart programming we have 61 keywords
+          i.e -> static, var, dynamic , final, const, string , int etc
+  */
+
+  //1)Int
+  int a;
+  a = 10;
+  print(a);
+
+  //2)Const -> Assign value once compile time
+  const int num = 5;
+  print(num);
+
+  //3)Final -> Assign value once run-time
+  final int f;
+  f = 2;
+  print(f);
+}
