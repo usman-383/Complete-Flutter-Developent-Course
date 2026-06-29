1dfa8bcefdb7184259cafb027f964e8c1e42052b
usman-383 <usmankhan.x383@gmail.com>
2026-06-29 08:59:13 +0500

    Create 9-Data-Type.dart

---
+++ Section-3 Dart Programming Fundamental/9-Data-Type.dart
@@ -0,0 +1,77 @@
+void main() {
+  //Data type:
+
+  //1)Int -> Use for whole numbers
+  //Declaration
+  //int a;
+
+  //Assigning
+  //a = 10;
+
+  //output
+  //print(a);
+
+  //2)Number -> Use for both floating and whole numbers
+  // ( range -32768 to 32767)
+  //Declaration
+  num num1;
+  num num2;
+
+  //Assigning
+  num1 = 10;
+  num2 = 10.5;
+
+  //output
+  print(num1);
+  print(num2);
+
+  //3)String -> Use for storing words or sentence.
+  //Declaration
+  String name;
+
+  //Assigning
+  name = "Usman";
+
+  //output
+  print("Welcome $name");
+
+  //4)Double -> Use for storing floating numbers
+  //Declaration
+  double d;
+
+  //Assigning
+  d = 10.0;
+
+  //Output
+  print(d);
+
+  //5)Char -> Use for storing single character
+  //Declaration
+  String ch;
+
+  //Assigning
+  ch = 'U';
+
+  //Output
+  print(ch);
+
+  //6)Boolean
+  //Declaration
+  bool isTrue;
+
+  //Assigning
+  isTrue = true;
+
+  //Output
+  print(isTrue);
+
+  //7)BigInt
+  //Declaration
+  BigInt int;
+
+  //Assigning
+  int = BigInt.parse('1234567890987665321');
+
+  //Output
+  print(int);
+}
