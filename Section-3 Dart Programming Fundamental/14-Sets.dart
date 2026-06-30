6a43eac31b224bc1e10f17ee67f383874c876e2f
usman-383 <usmankhan.x383@gmail.com>
2026-06-30 06:45:11 +0500

    Create 14-Set.dart

---
+++ Section-3 Dart Programming Fundamental/14-Sets.dart
@@ -0,0 +1,25 @@
+void main() {
+  /*
+  Sets: 
+      Store data in unorderd way. We store unique value in sets.
+  */
+
+  //Example
+  Set items = {1, 2, 3, 4, 5};
+  print(items);
+
+  Set items1 = {6, 7, 8, 9};
+  print(items1);
+
+  //Simple Commands
+  items.first;
+  items.add(6);
+  items.isEmpty;
+  items.isNotEmpty;
+  items.last;
+  items.length;
+  items.contains(6);
+  items.elementAt(0);
+  items.remove(3);
+  items.addAll(items1);
+}
