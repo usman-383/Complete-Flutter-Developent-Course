b4af8112eee8e5fea797de21fb34a19990e948d5
usman-383 <usmankhan.x383@gmail.com>
2026-06-30 15:32:01 +0500

    Create 20-OOPS.dart

---
+++ Section-3 Dart Programming Fundamental/20-OOPS.dart
@@ -0,0 +1,16 @@
+/*
+OOP:
+  Object oriented programming.
+  .)Less number of code
+  .)Improve reusability
+  .)Improve retandency.
+  .)Manage code
+
+  Main points.
+  1)Classes
+  2)Objects
+  3)Abstraction
+  4)Encapsulation
+  5)Inheritance
+  6)Polymorphism
+*/
