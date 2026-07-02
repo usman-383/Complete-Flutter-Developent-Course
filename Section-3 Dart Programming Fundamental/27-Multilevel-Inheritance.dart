828699db3ecc80506d1357f7cb8beae55fa80fb7
usman-383 <usmankhan.x383@gmail.com>
2026-07-02 22:49:24 +0500

    Update 27-Multilevel-Inheritance.dart

--- Section-3 Dart Programming Fundamental/27-Multilevel-Inheritance.dart
+++ Section-3 Dart Programming Fundamental/27-Multilevel-Inheritance.dart
@@ -1,3 +1,10 @@
+/*
+Mutlilevel Inheritance: In multilevel inheritance, a class is derived from
+ another derived class. It is a chain of inheritance. 
+ In this type of inheritance, the properties and methods of the base 
+ class and the derived class are inherited by the new derived class.
+*/
+
 class A {
   printA() {
     print("Class A");
