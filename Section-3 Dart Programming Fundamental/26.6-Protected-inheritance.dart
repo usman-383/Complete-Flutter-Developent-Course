7dae6c974beffdd4b3a8563f66a501dbc1e2df4a
usman-383 <usmankhan.x383@gmail.com>
2026-07-02 23:24:45 +0500

    Protected inheritance

---
+++ Section-3 Dart Programming Fundamental/26.6-Protected-inheritance.dart
@@ -0,0 +1,31 @@
+/*
+Dart does not have a real protected keyword like some other languages.
+
+Instead:
+
+use extends for inheritance
+annotate members with @protected from package:meta
+*/
+
+import 'package:';
+
+class Parent {
+  @protected
+  int protectedValue = 42;
+
+  void doSomething() {
+    print('Parent does something with $protectedValue');
+  }
+}
+
+class Child extends Parent {
+  void showValue() {
+    print('Child can access protectedValue: $protectedValue');
+  }
+}
+
+void main() {
+  final child = Child();
+  child.showValue();
+  // child.protectedValue; // analyzer warning if used outside subclass
+}
