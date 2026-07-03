0ec8009eaeca19ea5e0d7cf34bc44e61e20cecde
usman-383 <usmankhan.x383@gmail.com>
2026-07-03 09:42:22 +0500

    Setter Getter

---
+++ Section-3 Dart Programming Fundamental/28-Setter-Getter.dart
@@ -0,0 +1,24 @@
+/*
+Setter and Getter in Dart:
+                    Specifically, Dart provides a way to define custom behavior
+when getting or setting a property of an object. This is done using getter and 
+setter methods.
+
+.)They are special function that provide read & write access to the object properties.
+*/
+
+class Student {
+  String _name = "John"; // private variable
+
+  // Getter method
+  String get name {
+    return this._name; //Private variable is accessed using getter method
+  }
+
+  // Setter method
+  set name(String name) {
+    this._name = name;
+  }
+}
+
+void main() {}
