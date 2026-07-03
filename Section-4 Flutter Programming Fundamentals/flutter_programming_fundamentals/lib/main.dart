431ce3e9aa7d919acd5c2376f36c5be5c056f69e
usman-383 <usmankhan.x383@gmail.com>
2026-07-03 15:00:38 +0500

    Update file

--- Section-4 Flutter Programming Fundamentals/flutter_programming_fundamentals/lib/main.dart
+++ Section-4 Flutter Programming Fundamentals/flutter_programming_fundamentals/lib/main.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
+
+  print(
+    "Hello, World!",
+  ); // Print statement to display a message in the console.
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  //Variables: Used for storing different data types and values in the application.
+  String name = "Usman dev";
+  int age = 20;
+  double height = 5.9;
+  bool isStudent = true;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Scaffold());
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
   }
 }
