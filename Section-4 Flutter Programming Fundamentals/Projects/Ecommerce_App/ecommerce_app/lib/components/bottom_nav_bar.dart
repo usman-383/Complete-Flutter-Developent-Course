452d9c658c938acb325724a61a15166e9fde30e2
usman-383 <usmankhan.x383@gmail.com>
2026-07-07 14:43:29 +0500

    update

--- Section-4 Flutter Programming Fundamentals/Projects/Ecommerce_App/ecommerce_app/lib/components/bottom_nav_bar.dart
+++ Section-4 Flutter Programming Fundamentals/Projects/Ecommerce_App/ecommerce_app/lib/components/bottom_nav_bar.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
 class MyBottomNavBar extends StatelessWidget {
-  void Function{int}? onTabChange;
-  MyBottomNavBar({super.key,required this.onTabChange});
+  final ValueChanged<int>? onTabChange;
+
+  const MyBottomNavBar({super.key, this.onTabChange});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(vertical: 20),
+      padding: const EdgeInsets.symmetric(vertical: 20),
       child: GNav(
         color: Colors.grey[400],
         activeColor: Colors.grey.shade700,
@@ -16,8 +17,7 @@ class MyBottomNavBar extends StatelessWidget {
         tabBackgroundColor: Colors.grey.shade100,
         mainAxisAlignment: MainAxisAlignment.center,
         tabBorderRadius: 16,
-        onTabChange: ((value) => onTabChange,
-
+        onTabChange: onTabChange,
         tabs: const [
           GButton(icon: Icons.home, text: 'Shop'),
           GButton(icon: Icons.shopping_bag_rounded, text: 'Cart'),
--- Section-4 Flutter Programming Fundamentals/Projects/Ecommerce_App/ecommerce_app/lib/pages/home_page.dart
+++ Section-4 Flutter Programming Fundamentals/Projects/Ecommerce_App/ecommerce_app/lib/pages/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:ecommerce_app/components/bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
 
+import 'cart_page.dart';
+import 'shop_page.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -9,11 +12,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<Widget> _pages = [const ShopPage(), const CartPage()];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      
-      bottomNavigationBar: const MyBottomNavBar());
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: MyBottomNavBar(onTabChange: navigateBottomBar),
+    );
   }
 }
