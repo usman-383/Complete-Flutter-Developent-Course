8364faf6099623be37d7f90b2807a17218c17056
usman-383 <usmankhan.x383@gmail.com>
2026-07-07 15:21:32 +0500

    update shop_page

---
+++ Section-4 Flutter Programming Fundamentals/Projects/Ecommerce_App/ecommerce_app/lib/pages/shop_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class ShopPage extends StatefulWidget {
+  const ShopPage({super.key});
+
+  @override
+  State<ShopPage> createState() => _ShopPageState();
+}
+
+class _ShopPageState extends State<ShopPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        //search bar
+        Container(
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(horizontal: 25),
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text('Search', style: TextStyle(color: Colors.grey)),
+              Icon(Icons.search, color: Colors.grey),
+            ],
+          ),
+        ),
+
+        //message
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 25.0),
+          child: Text(
+            'Everyone flies......some fly longer that others',
+            style: TextStyle(color: Colors.grey[600]),
+          ),
+        ),
+
+        //hot picks
+      ],
+    );
+  }
+}
