5f4758e3f9790461aa0d0923d5196dd623d4b080
usman-383 <usmankhan.x383@gmail.com>
2026-07-07 15:05:39 +0500

    update home_page

--- Section-4 Flutter Programming Fundamentals/Projects/Ecommerce_App/ecommerce_app/lib/pages/home_page.dart
+++ Section-4 Flutter Programming Fundamentals/Projects/Ecommerce_App/ecommerce_app/lib/pages/home_page.dart
@@ -25,6 +25,73 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+              icon: const Padding(
+                padding: EdgeInsets.only(left: 12.0),
+                child: Icon(Icons.menu, color: Colors.black),
+              ),
+            );
+          },
+        ),
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.grey[900],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            //logo
+            Column(
+              children: [
+                DrawerHeader(
+                  child: Image.asset(
+                    'lib/images/logo.png',
+                    color: Colors.white,
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Divider(color: Colors.grey[800]),
+                ),
+
+                //other pages
+                const Padding(
+                  padding: const EdgeInsets.only(left: 25.0),
+                  child: ListTile(
+                    leading: Icon(Icons.home, color: Colors.white),
+                    title: Text('Home', style: TextStyle(color: Colors.white)),
+                  ),
+                ),
+
+                const Padding(
+                  padding: const EdgeInsets.only(left: 25.0),
+                  child: ListTile(
+                    leading: Icon(Icons.info, color: Colors.white),
+                    title: Text('About', style: TextStyle(color: Colors.white)),
+                  ),
+                ),
+              ],
+            ),
+
+            const Padding(
+              padding: const EdgeInsets.only(left: 25.0, bottom: 25.0),
+              child: ListTile(
+                leading: Icon(Icons.logout, color: Colors.white),
+                title: Text('Logout', style: TextStyle(color: Colors.white)),
+              ),
+            ),
+          ],
+        ),
+      ),
       backgroundColor: Colors.grey[300],
       body: _pages[_selectedIndex],
       bottomNavigationBar: MyBottomNavBar(onTabChange: navigateBottomBar),
