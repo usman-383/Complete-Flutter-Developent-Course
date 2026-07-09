d89f54559cc28c7d5b2e2a69524a656829d4d2ea
usman-383 <usmankhan.x383@gmail.com>
2026-07-09 13:16:15 +0500

    Update config.dart

--- Section-4 Flutter Programming Fundamentals/Projects/Ecommerce_App/ecommerce_app/lib/config.dart
+++ Section-4 Flutter Programming Fundamentals/Projects/Ecommerce_App/ecommerce_app/lib/config.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/foundation.dart';
 
-// Override with --dart-define=SERVER_BASE_URL=... for physical devices.
+// Override with --dart-define=SERVER_BASE_URL=... if you need a different host.
 String get serverBaseUrl {
-  const override = String.fromEnvironment('SERVER_BASE_URL');
+  const override = String.fromEnvironment(
+    'SERVER_BASE_URL',
+    defaultValue: 'http://192.168.100.16:3000',
+  );
   if (override.isNotEmpty) {
     return override;
   }
@@ -11,12 +14,9 @@ String get serverBaseUrl {
     return 'http://localhost:3000';
   }
 
-  switch (defaultTargetPlatform) {
-    case TargetPlatform.android:
-      return 'http://10.0.2.2:3000';
-    default:
-      return 'http://localhost:3000';
-  }
+  return defaultTargetPlatform == TargetPlatform.android
+      ? 'http://10.0.2.2:3000'
+      : 'http://192.168.100.16:3000';
 }
 
 // Put your Stripe publishable key here (pk_test_...)
