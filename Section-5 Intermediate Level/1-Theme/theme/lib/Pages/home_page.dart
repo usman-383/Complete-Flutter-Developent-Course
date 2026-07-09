48032cb434a9b55aa977141cca4017071c76a205
usman-383 <usmankhan.x383@gmail.com>
2026-07-09 15:34:29 +0500

    Update home_page.dart

--- Section-5 Intermediate Level/1-Theme/theme/lib/Pages/home_page.dart
+++ Section-5 Intermediate Level/1-Theme/theme/lib/Pages/home_page.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:theme/Components/button.dart';
+import '../Components/box.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
+      backgroundColor: Colors.deepPurple[500],
       appBar: AppBar(title: const Text('Theme Demo')),
-      body: const Center(child: Text('Hello, World!')),
+      body: Center(child: MyBox(
+        color: Colors.deepPurple[300],
+        child: MyButton(color:Colors.deepPurple[200],onTap: (){();},
+      ),),
     );
   }
 }
