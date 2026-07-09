43bdb66252d4bd92c22a59f76bdb135570d7d027
usman-383 <usmankhan.x383@gmail.com>
2026-07-09 15:14:27 +0500

    Update

--- Section-5 Intermediate Level/1-Theme/theme/lib/main.dart
+++ Section-5 Intermediate Level/1-Theme/theme/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'Pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const MyApp(),
+
+      home: HomePage();
     );
   }
 }
