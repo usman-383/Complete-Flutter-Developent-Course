b9d0ca818447bc4729d562cbfb291b11ba774940
usman-383 <usmankhan.x383@gmail.com>
2026-07-23 09:09:25 +0500

    Update home page

--- Section-5 Intermediate Level/2-State Management/Counter App/counter_app/lib/home_page.dart
+++ Section-5 Intermediate Level/2-State Management/Counter App/counter_app/lib/home_page.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // final String title;
+  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
